fedcba2f9d6d10de756e87beb440f62ac722c9d0
Jim van der Voort <jim@jim.computer>
2024-09-05 13:45:44 +0200

    Find lat/lon for addresses on import

--- homes/management/commands/import.py
+++ homes/management/commands/import.py
@@ -1,8 +1,19 @@
 import csv
+import time
 
 from django.core.management.base import BaseCommand
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 
 from homes.models import Home
+
+
+def find_address(address):
+    geolocator = Nominatim(user_agent='Jimmy')
+    location = geolocator.geocode(address)
+    if location:
+        return location.latitude, location.longitude
+    return None
 
 
 def update_home(home, row):
@@ -23,7 +34,7 @@ def update_home(home, row):
 
 
 def create_home(row):
-    Home.objects.create(
+    return Home.objects.create(
         url=row['URL'],
         address=row['Address'],
         city=row['City'],
@@ -51,18 +62,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         csv_file = options['csv_file']
+
         with open(csv_file, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 home = Home.objects.filter(url=row['URL']).first()
+
                 if home:
                     update_home(home, row)
-                    continue
+                else:
+                    try:
+                        home = create_home(row)
+                    except Exception as e:
+                        self.stdout.write(self.style.ERROR(f'Error importing home on row {reader.line_num}: {e}'))
+                        continue
 
-                try:
-                    create_home(row)
-                except Exception as e:
-                    self.stdout.write(self.style.ERROR(f'Error importing home on row {reader.line_num}: {e}'))
-                    continue
+                if home.lat == 0 and home.lon == 0:
+                    try:
+                        location = find_address(f"{home.address}, {home.city}")
+                    except GeocoderTimedOut:
+                        self.stdout.write(self.style.WARNING(f'Geocoder timed out for row, waiting 10 seconds'))
+                        time.sleep(10)
+
+                    if location:
+                        self.stdout.write(self.style.SUCCESS(
+                            f'Found location for row {reader.line_num}: {home.address}, {home.city}'))
+                        home.lat, home.lon = location
+                        home.save()
+                    else:
+                        self.stdout.write(self.style.WARNING(
+                            f'Could not find location for row {reader.line_num}: {home.address}, {home.city}'))
 
         self.stdout.write(self.style.SUCCESS(f'Processed {reader.line_num} homes'))
